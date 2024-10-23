64a5383ed4a1f40f2dde05c73b772ab3df13eb20
ashuu03449 <codewithashu94@gmail.com>
2024-10-23 03:31:50 -0700

    change name

--- Milestone1_2/script.js
+++ Milestone1_2/script.js
@@ -1,9 +1,10 @@
-var toggleButton = document.getElementById('toggleSkills');
-var skillsSection = document.getElementById('skills');
+"use strict";
+const toggleButton = document.getElementById('toggleSkills');
+const skillsSection = document.getElementById('skills');
 // Initially show the skills section
 skillsSection.style.display = 'block';
 toggleButton.textContent = 'Hide Skills'; // Initial button text
-toggleButton.addEventListener('click', function () {
+toggleButton.addEventListener('click', () => {
     if (skillsSection.style.display === 'none') {
         skillsSection.style.display = 'block';
         toggleButton.textContent = 'Hide Skills'; // Update button text
